2c78e82c7d608844bf4dbcd7612dbf7d112132fe
Dat Tran <dat.h.tran@eastagile.com>
2024-01-17 17:05:44 +0700

    Create generating broadcast api
    Co-authored-by: Ky Phan <ky.phan@eastagile.com>

--- supabase/functions/backend/routes/Api.ts
+++ supabase/functions/backend/routes/Api.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import Paths from '../constants/Paths.ts';
 import UserController from '../controllers/UserController.ts';
+import broadcastController from "../controllers/BroadcastController.ts";
 
 
 const apiRouter = Router();
 const userRouter = Router();
+const broadcastRouter = Router();
 
 // Get all users
 userRouter.get(
@@ -13,6 +15,12 @@ userRouter.get(
   UserController.getAll,
 );
 
+broadcastRouter.post(
+    Paths.Broadcast.Make,
+    broadcastController.make,
+);
+
 apiRouter.use(Paths.Users.Base, userRouter);
+apiRouter.use(Paths.Broadcast.Base, broadcastRouter);
 
 export default apiRouter;
