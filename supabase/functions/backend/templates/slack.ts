b5470ac6494295d787f3567d59c2665d201302ea
Dat Tran <dat.h.tran@eastagile.com>
2024-01-19 14:54:13 +0700

    [f] add sending to slack Co-authored-by: Ky Phan <ky.phan@eastagile.com>

---
+++ supabase/functions/backend/templates/slack.ts
@@ -0,0 +1,7 @@
+export const markdownTemplate = `
+:warning: *Failed Making Broadcast Message* :warning:
+
+The server received a broadcast making request that failed due to the following reason:
+- *<%= failureDetails %>*
+`;
+
