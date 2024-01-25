25ca5b68b94f9db5fa3d2a87d633c680a2ca2d9c
Dat Tran <dat.h.tran@eastagile.com>
2024-01-25 17:33:21 +0700

    [f] Implement APIs for Frontend

--- supabase/functions/backend/misc/AppResponse.ts
+++ supabase/functions/backend/misc/AppResponse.ts
@@ -6,6 +6,20 @@ const unauthorized = (res: Response) => {
   return res.status(401).json({ message: USER_UNAUTHORIZED_ERR });
 };
 
+export const intervalToString = (interval: string) => {
+  const [hours, minutes, seconds] = interval.split(":").map(Number);
+
+  if (hours > 0) {
+    return `${hours} ${hours === 1 ? "hour" : "hours"}`;
+  } else if (minutes > 0) {
+    return `${minutes} ${minutes === 1 ? "minute" : "minutes"}`;
+  } else if (seconds > 0) {
+    return `${seconds} ${seconds === 1 ? "second" : "seconds"}`;
+  } else {
+    return "Invalid interval format";
+  }
+};
 export default {
   unauthorized,
+  intervalToString,
 } as const;
