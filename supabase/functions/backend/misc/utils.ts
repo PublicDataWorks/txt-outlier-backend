5b1d9419e437e8e86454c13d93085d6d86d1555f
Dat T + Ky P <ky.phan@eastagile.com>
2024-02-15 17:03:44 +0700

    [f] OUT-77 - Add more tests
    [OUT-77](https://linear.app/pdw/issue/OUT-77/check-for-reply-in-10-minutes-timeframe-after-first-message)
    
    Co-authored-by: Dat Tran <dat.h.tran@eastagile.com>

--- supabase/functions/backend/misc/utils.ts
+++ supabase/functions/backend/misc/utils.ts
@@ -1,14 +1,19 @@
 const intervalToString = (interval: string) => {
   const [hours, minutes, seconds] = interval.split(':').map(Number)
+  let result = ''
 
-  if (hours > 0) {
-    return `${hours} ${hours === 1 ? 'hour' : 'hours'}`
-  } else if (minutes > 0) {
-    return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`
-  } else if (seconds > 0) {
-    return `${seconds} ${seconds === 1 ? 'second' : 'seconds'}`
-  } else {
+  if (hours > 0) result += `${hours} ${hours === 1 ? 'hour' : 'hours'}, `
+  if (minutes > 0) result += `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}, `
+  if (seconds > 0) result += `${seconds} ${seconds === 1 ? 'second' : 'seconds'}`
+
+  if (result === '') {
     return 'Invalid interval format'
+  } else {
+    result = result.trim()
+    if (result.endsWith(',')) {
+      result = result.slice(0, -1)
+    }
+    return result
   }
 }
 
