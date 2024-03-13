60014397635964c8fe405f788d657e41d197eaac
Dat T + Ky P <ky.phan@eastagile.com>
2024-03-13 16:25:32 +0700

    [b] OUT-106 - Escape query with user input
    [OUT-106](https://linear.app/pdw/issue/OUT-106/escape-apostrophe-when-insert-new-outgoing-messages)
    
    Co-authored-by: Dat Tran <dat.h.tran@eastagile.com>

---
+++ supabase/functions/backend/scheduledcron/helpers.ts
@@ -0,0 +1,18 @@
+// deno-lint-ignore no-explicit-any
+export const escapeLiteral = (val: any): string => {
+  if (!val) {
+    return 'NULL'
+  }
+
+  if (Array.isArray(val)) {
+    const vals = val.map(escapeLiteral)
+    return '(' + vals.join(', ') + ')'
+  }
+
+  const backslash = val.indexOf('\\') !== -1
+  const prefix = backslash ? 'E' : ''
+  val = val.replace(/'/g, "''")
+  val = val.replace(/\\/g, '\\\\')
+
+  return prefix + "'" + val + "'"
+}
