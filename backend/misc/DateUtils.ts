2af65041ce1bdaee79bfebf6f40a75511de43af2
Hung Ta <hung.ta@eastagile.com>
2024-05-10 15:16:02 +0700

    [f] - Update new timestamp when send next broadcast
    Co-authored-by: DatT <dat.h.tran@eastagile.com>

--- backend/misc/DateUtils.ts
+++ backend/misc/DateUtils.ts
@@ -9,4 +9,22 @@ const diffInMinutes = (runAt: Date): number => {
   return (runAt.getTime() - now.getTime()) / (1000 * 60)
 }
 
-export default { advance, diffInMinutes }
+const getNextTimestamp = (date = new Date()) => {
+  const now = new Date(date)
+  const day = now.getUTCDay()
+
+  let nextDay
+  if (day === 1 || day === 2) {
+    // If it's Monday, Tuesday, or it's Wednesday and it's before 10 a.m. EDT
+    nextDay = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + (3 - day), 14, 0, 0))
+  } else if (day === 3 || day === 4) {
+    // If it's Wednesday, Thursday, or it's Friday and it's before 10 a.m. EDT
+    nextDay = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + (5 - day), 14, 0, 0))
+  } else {
+    // If it's Friday after 10 a.m. EDT, or it's Saturday or Sunday
+    nextDay = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + (8 - day), 14, 0, 0))
+  }
+  return nextDay
+}
+
+export default { advance, diffInMinutes, getNextTimestamp }
---
+++ backend/tests/utils/getNextTimestamp.test.ts
@@ -0,0 +1,49 @@
+import { assertEquals } from 'testing/asserts.ts'
+import { describe, it } from 'testing/bdd.ts'
+import dateUtils from '../../misc/DateUtils.ts'
+
+describe('getNextTimestamp', () => {
+  describe('given that the input is Monday before 10 a.m. EDT', () => {
+    it('returns next Wednesday at 10 a.m. EDT', () => {
+      const date = new Date('2022-12-05T13:00:00Z') // This is Monday 9 a.m. EDT
+      const result = dateUtils.getNextTimestamp(date)
+
+      assertEquals(result.getUTCDay(), 3)
+      assertEquals(result.getUTCHours(), 14)
+      assertEquals(result.toISOString(), '2022-12-07T14:00:00.000Z')
+    })
+  })
+
+  describe('given that the input is Tuesday', () => {
+    it('returns next Wednesday at 10 a.m. EDT', () => {
+      const date = new Date('2022-12-06T15:00:00Z') // This is Tuesday 10 a.m. EDT
+      const result = dateUtils.getNextTimestamp(date)
+
+      assertEquals(result.getUTCDay(), 3)
+      assertEquals(result.getUTCHours(), 14)
+      assertEquals(result.toISOString(), '2022-12-07T14:00:00.000Z')
+    })
+  })
+
+  describe('given that the input is Friday after 10 a.m. EDT', () => {
+    it('returns next Monday at 10 a.m. EDT', () => {
+      const date = new Date('2022-12-09T15:00:00Z') // This is Friday 11 a.m. EDT
+      const result = dateUtils.getNextTimestamp(date)
+
+      assertEquals(result.getUTCDay(), 1)
+      assertEquals(result.getUTCHours(), 14)
+      assertEquals(result.toISOString(), '2022-12-12T14:00:00.000Z')
+    })
+  })
+
+  describe('given that the input is Saturday', () => {
+    it('returns next Monday at 10 a.m. EDT', () => {
+      const date = new Date('2022-12-10T15:00:00Z') // This is Saturday 10 a.m. EDT
+      const result = dateUtils.getNextTimestamp(date)
+
+      assertEquals(result.getUTCDay(), 1)
+      assertEquals(result.getUTCHours(), 14)
+      assertEquals(result.toISOString(), '2022-12-12T14:00:00.000Z')
+    })
+  })
+})
