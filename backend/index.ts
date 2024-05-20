4bc7418b9b48a91ca402f9b59a44875422f85321
Hung Ta <hung.ta@eastagile.com>
2024-05-20 08:44:40 +0700

    [f] - Set up cron to send weekly report
    Co-authored-by: HungT <hung.ta@eastagile.com>

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,7 @@
 import server from './server.ts'
 import * as log from 'log'
+import cron from 'node-cron'
+import AnalyticsService from './services/AnalyticsService.ts'
 
 const port = 8000
 
@@ -7,3 +9,11 @@ const SERVER_START_MSG = 'Express server started on port: ' +
   port.toString()
 
 server.listen(port, () => log.info(SERVER_START_MSG))
+
+// const task = cron.schedule('* * * * *', () => {
+//   AnalyticsService.sendWeeklyReport()
+// }, {
+//   scheduled: false,
+// })
+
+// task.start()
--- backend/services/AnalyticsService.ts
+++ backend/services/AnalyticsService.ts
@@ -8,6 +8,16 @@ async function generateWeeklyAnalyticsReport() {
   return unsubscribedMessages
 }
 
+async function sendWeeklyReport() {
+  const report = await generateWeeklyAnalyticsReport()
+  // Convert the report to a string or format it as needed
+  const reportMessage = JSON.stringify(report)
+  // Replace 'phone_number' with the phone number of the Missive conversation
+  const toPhone = 'phone_number'
+  MissiveUtils.sendMessage(reportMessage, toPhone)
+}
+
 export default {
   generateWeeklyAnalyticsReport,
+  sendWeeklyReport,
 }
